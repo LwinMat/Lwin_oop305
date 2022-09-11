016bc66d6b85b54f09ffb1f29b72456e398f964c
Lwin Mateo Lopez <lymateo-lopez@myseneca.ca>
2022-09-11 17:24:07 -0400

    Adding my first file of oop305

---
+++ btp305_week1/week1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+constexpr int getSize(int seed) {
+    return seed * 2;
+}
+
+//int main() {
+int main(int argc, char** argv) {
+    //char str[]{"hello"};
+    for (int i = 0; i < argc; ++i)
+        cout << i << ". " << argv[i] << endl;
+
+
+
+    // const: value cannot change after creation
+    // constexpr: a constant + value must be known at compile time
+    //int iSize{};
+    //cout << "Size? ";
+    //cin >> iSize;
+
+    //const int size = iSize;
+    //constexpr int SIZE = size;
+    //double arr[SIZE]{};
+
+    // double arr[getSize(iSize)]{};
+}
