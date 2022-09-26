8d5e3be4f383d03b9018e107110791b792684913
LwinMateo <97479596+LwinMateo@users.noreply.github.com>
2022-09-25 23:25:47 -0400

    Add files via upload
    More coding example

---
+++ Another_Polymorphism/Shape.h
@@ -0,0 +1,13 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+// Polymorphic Objects - Dynamic Cast
+// Shape.h
+
+class Shape
+{
+public:
+    virtual double volume() const = 0;
+    virtual void display() const = 0;
+    virtual bool operator==(const Shape&) const = 0;
+};
+#endif
